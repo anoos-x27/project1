c41dd40373acf731ec660bcc3789825327a1ff2e
Habiba <habibaaf07@gmail.com>
2025-11-26 22:55:08 +0200

    added print "hello world"

--- Coding task.1.py
+++ Coding task.1.py
@@ -1,8 +1,7 @@
-x =  'Go to the next room and shut the door, while doing so donot forget the keys, if got your parents keys donot forget to close the windows as well'  
+x =  'Go to THE next room and shut the door, while doing so donot forget the keys, if got your parents keys donot forget to close the windows as well'  
 word_tuple = tuple(x)
 print(word_tuple)
 print("Words containing 'o'" )
 for word in word_tuple:
    if  'o' in word_tuple:
       print("Hello")
-   print("hello world")
